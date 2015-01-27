8e7e888d38978fb4e2663d15353cdb53c60457b6
Elijah Kim <kimelijah90@gmail.com>
2015-01-26 20:19:21 -0500

    Turn tags into links

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ActiveRecord::Base
   belongs_to :user
 
-  has_many :taggings
+  has_many :taggings, dependent: :destroy
   has_many :tags, through: :taggings
 
   validates :title, presence: true
